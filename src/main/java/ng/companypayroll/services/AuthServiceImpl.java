333825fa98393366b14f7f6a75f221b8921569c3
Adedokun Israel <adedokunisrael19@gmail.com>
2026-09-12 22:03:12 +0100

    tested the authservice

--- src/main/java/ng/companypayroll/services/AuthServiceImpl.java
+++ src/main/java/ng/companypayroll/services/AuthServiceImpl.java
@@ -5,6 +5,7 @@ import ng.companypayroll.data.repository.UserRepository;
 import ng.companypayroll.dto.request.LoginRequest;
 import ng.companypayroll.dto.response.LoginResponse;
 import ng.companypayroll.dto.response.UserResponse;
+import ng.companypayroll.exceptions.InvalidCredentials;
 import ng.companypayroll.exceptions.UserNotFoundException;
 import ng.companypayroll.utils.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,12 @@ public class AuthServiceImpl {
     @Autowired
     private UserRepository userRepository;
 
-    public UserResponse login(LoginRequest request) {
+    public UserResponse login(LoginRequest request) throws InvalidCredentials {
         User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() ->
                         new UserNotFoundException("User not found"));
         if(!user.getPassword().equals(request.getPassword())) {
-            throw new RuntimeException("Incorrect  Username or password");
+            throw new InvalidCredentials("Incorrect  Username or password");
         }
 
         return Mapper.map(user);
--- src/test/java/ng/companypayroll/services/AuthServiceImplTest.java
+++ src/test/java/ng/companypayroll/services/AuthServiceImplTest.java
@@ -1,7 +1,92 @@
 package ng.companypayroll.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ng.companypayroll.data.model.Role;
+import ng.companypayroll.data.model.User;
+import ng.companypayroll.data.repository.UserRepository;
+import ng.companypayroll.dto.request.LoginRequest;
+import ng.companypayroll.dto.response.LoginResponse;
+import ng.companypayroll.dto.response.UserResponse;
+import ng.companypayroll.exceptions.InvalidCredentials;
+import ng.companypayroll.exceptions.UserNotFoundException;
+import ng.companypayroll.utils.Mapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class AuthServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private Mapper mapper;
+
+    @InjectMocks
+    private AuthServiceImpl authService;
+
+    @Test
+    public void testThatUserShouldBeAbleToLogin() throws InvalidCredentials {
+        User user = new User();
+        user.setId("1");
+        user.setFullName("John Doe");
+        user.setEmail("john@gmail.com");
+        user.setPassword("1234");
+        user.setRole(Role.EMPLOYEE);
+
+        LoginRequest request = new LoginRequest();
+        request.setEmail("john@gmail.com");
+        request.setPassword("1234");
+
+        LoginResponse response = new LoginResponse();
+        response.setId("1");
+        response.setFullName("John Doe");
+        response.setEmail("john@gmail.com");
+        response.setRole(Role.EMPLOYEE);
+
+        when(userRepository.findByEmail(request.getEmail())).thenReturn(Optional.of(user));
+
+        UserResponse result = authService.login(request);
+
+        assertEquals("1", result.getId());
+        assertEquals("John Doe", result.getFullName());
+        assertEquals(Role.EMPLOYEE, result.getRole());
+
+    }
+
+    @Test
+    public void ExceptionShouldbeThrownwhenUserDoesNotExist(){
+      LoginRequest request = new LoginRequest();
+
+      request.setEmail("JohnDoe@gmail.com");
+      request.setPassword("1234");
+
+      when(userRepository.findByEmail(request.getEmail())).thenReturn(Optional.empty());
+
+      assertThrows(UserNotFoundException.class, () -> authService.login(request));
+    }
+
+
+    @Test
+    public void ExceptionShouldbeThrownwhenPasswordDoesNotMatch(){
+        User user = new User();
+        user.setId("1");
+        user.setEmail("john@gmail.com");
+        user.setPassword("1234");
+
+        LoginRequest request = new LoginRequest();
+        request.setEmail("john@gmail.com");
+        request.setPassword("wrong");
+
+        assertThrows(InvalidCredentials.class, ()-> authService.login(request));
+
+    }
 
 }
